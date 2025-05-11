5c0fe1ba9ce97e54b77d8c44d62e28a39bcad53e
Tushar <td220627@gmail.com>
2025-05-11 15:47:45 +0530

    Data Validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tproj.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from tproj.pipelines.stage_02_data_validation import DataValidationTrainingPipeline
+from tproj.pipelines.stage_03_data_transformation import DataTransformationTrainingPipeline
 from tproj.logging import logger
 
 
@@ -18,6 +19,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
